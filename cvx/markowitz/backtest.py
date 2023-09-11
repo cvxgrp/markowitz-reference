59f52e8ca439f73afec34eb4d137be1847fcf918
Kasper Johansson <kasperjo@kth.se>
2023-09-11 12:48:07 -0700

    edits

--- cvx/markowitz/backtest.py
+++ cvx/markowitz/backtest.py
@@ -1,5 +1,28 @@
 import pandas as pd
 import cvxpy as cp
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class RiskModel:
+    def __init__(self, factor_covariance, idio_vola, factor_exposures):
+        """
+        param factor_covariance: dictionary of (n_factors, n_factors) covariance
+            matrices, where the keys are the dates
+        param idio_vola: dataframe of idiosyncratic volatilities,
+            where columns are assets and rows are the dates
+        param factor_exposures: dictionary of factor exposures, where the keys
+            are the dates
+        """
+        # make sure dates align
+        assert set(factor_covariance.keys()) == set(idio_vola.index), \
+            "factor_covariance and idio_vola must have the same dates"
+        assert set(factor_covariance.keys()) == set(factor_exposures.keys()), \
+            "factor_covariance and factor_exposures must have the same dates"
+
+        self.factor_covariance = factor_covariance
+        self.idio_vola = idio_vola
+        self.factor_exposures = factor_exposures
+        self.times = sorted(factor_covariance.keys())
 
 
 
@@ -7,29 +30,41 @@ import cvxpy as cp
 def markowitz_problem(n_assets, n_factors):
 
     ### Variables
-    asset_weights = cp.Variable(n_assets)
-    cash_weight = cp.Variable()
+    asset_weights = cp.Variable(n_assets, name="asset_weights")
+    cash_weight = cp.Variable(name="cash_weight")
 
     ### Data
-    asset_weights_prev = cp.Parameter(n_assets)
-    cash_weight_prev = cp.Parameter()
-    factor_weights = cp.Parameter(n_factors)
-    exposures = cp.Parameter((n_assets, n_factors))
+    asset_weights_prev = cp.Parameter(n_assets, name="asset_weights_prev")
+    cash_weight_prev = cp.Parameter(name="cash_weight_prev")
+    factor_weights = cp.Parameter(n_factors, name="factor_weights")
+    idio_mean = cp.Parameter(n_assets, name="idio_mean")
+    factor_mean = cp.Parameter(n_factors, name="factor_mean")
+    risk_free = cp.Parameter(name="risk_free")
+    factor_covariance_chol = cp.Parameter((n_factors, n_factors), name="factor_covariance_chol")
+    idio_vola = cp.Parameter(n_assets, name="idio_vola")
+    exposures = cp.Parameter((n_assets, n_factors), name="exposures")
+    kappa_short = cp.Parameter(n_assets, nonneg=True, name="kappa_short")   
+    kappa_brw = cp.Parameter(nonneg=True, name="kappa_brw")
+    kappa_bas = cp.Parameter(n_assets, nonneg=True, name="kappa_bas")
+    kappa_mi = cp.Parameter(n_assets, nonneg=True, name="kappa_mi")
+    
 
     ### Parameters
-    idio_mean = cp.Parameter(n_assets)
-    factor_mean = cp.Parameter(n_factors)
-    cash_mean = cp.Parameter()
-    factor_covariance_chol = cp.Parameter((n_factors, n_factors))
-    idio_vola = cp.Parameter(n_assets)
-
-    rho_mean = cp.Parameter(nonneg=True)
-    rho_covariance = cp.Parameter(nonneg=True)
-
-    gamma_hold = cp.Parameter(nonneg=True)
-    gamma_trade = cp.Parameter(nonneg=True)
-    gamma_turn = cp.Parameter(nonneg=True)
-    gamma_risk = cp.Parameter(nonneg=True)
+    assets_lower = cp.Parameter(n_assets, name="weights_lower")
+    assets_upper = cp.Parameter(n_assets, name="weights_upper")
+    cash_lower = cp.Parameter(name="cash_lower")
+    cash_upper = cp.Parameter(name="cash_upper")
+    trade_lower = cp.Parameter(n_assets, name="trade_lower")
+    trade_upper = cp.Parameter(n_assets, name="trade_upper")
+    turnover_limit = cp.Parameter(nonneg=True, name="turnover_limit")
+    leverage_limit = cp.Parameter(nonneg=True, name="leverage_limit")
+    rho_mean = cp.Parameter(nonneg=True, name="rho_mean")
+    rho_covariance = cp.Parameter(nonneg=True, name="rho_covariance")
+    gamma_hold = cp.Parameter(nonneg=True, name="gamma_hold")
+    gamma_trade = cp.Parameter(nonneg=True, name="gamma_trade")
+    gamma_turn = cp.Parameter(nonneg=True, name="gamma_turn")
+    gamma_risk = cp.Parameter(nonneg=True, name="gamma_risk")
+    risk_target = cp.Parameter(nonneg=True, name="risk_target")
 
     ### DPP enabling parameters
     variances = cp.sum(
@@ -38,9 +73,12 @@ def markowitz_problem(n_assets, n_factors):
         ), axis=1
     ) + idio_vola**2
     volas_times_sqrt_rho = cp.CallbackParam(callback=lambda:
-                                             variances.value**0.5 * rho_covariance.value**0.5)
-    _abs_weights = cp.Variable(n_assets, nonneg=True)
+                                             variances.value**0.5 * rho_covariance.value**0.5, name="volas_times_sqrt_rho")
+    _abs_weights = cp.Variable(n_assets, nonneg=True, name="_abs_weights")
 
+
+    ### Return and risk
+    return_wc = factor_mean@factor_weights + idio_mean@asset_weights + risk_free*cash_weight - rho_mean @ cp.abs(asset_weights)
 
     risk = cp.norm2(
         cp.hstack(
@@ -60,76 +98,37 @@ def markowitz_problem(n_assets, n_factors):
         )
     )
 
-    mean_wc = factor_mean@factor_weights + idio_mean@asset_weights + cash_mean*cash_weight - rho_mean @ cp.abs(asset_weights)
-
-
-
-
-
-
     
+    ### Costs
+    holding_cost = kappa_short @ cp.pos(-asset_weights) + kappa_brw*cp.pos(-cash_weight)
+    trading_cost = kappa_bas @ cp.abs(asset_weights - asset_weights_prev) + kappa_mi @ cp.abs(asset_weights - asset_weights_prev) + kappa_bas*cp.power(cash_weight - cash_weight_prev, 3/2)
 
+    ### Turnover and leverage
+    turnover = cp.norm1(asset_weights - asset_weights_prev) 
+    leverage = cp.norm1(asset_weights)
 
-
-def mean_var_factor(
-n, k, sigma_tar, lower=-0.3, upper=0.4, leverage=2, lower_cash=None, upper_cash=None
-):
-    """
-    Mean variance optimization with factor model
-    """
-
-    w = cp.Variable(n + 1, name="w")  # Last element is cash
-    f = cp.Variable(k, name="f")
-
-    w_old = cp.Parameter(n + 1, name="w_old")
-
-    alpha_w = cp.Parameter(n, name="alpha_w")
-    alpha_f = cp.Parameter(k, name="alpha_f")
-    exposure = cp.Parameter((n, k), name="exposure")
-    chol_f = cp.Parameter((k, k), name="chol_f")
-    idio_vola = cp.Parameter(n, name="idio_vola")
-    trading_costs = cp.Parameter(n, name="trading_costs")
-    trading_costs_times_w_old = cp.Parameter(n, name="trading_costs_times_w_old")
-
-    # if transaction_costs:
-    ret = (
-        alpha_f @ f
-        + alpha_w @ w[:-1]
-        - cp.norm1(cp.multiply(trading_costs, w[:-1]) - trading_costs_times_w_old)
+    ### Objective
+    obj = (
+        return_wc
+        - gamma_risk * cp.pos(risk_wc - risk_target)
+        - gamma_hold * holding_cost
+        - gamma_trade * trading_cost
+        - gamma_turn * cp.pos(turnover - turnover_limit)
     )
-    # else:
-    #     ret = alpha_f @ f + alpha_w @ w[:-1]
 
-    obj = cp.Maximize(ret)
+    ### Constraints
+    constraints = [
+        leverage<=leverage_limit,
+        asset_weights >= assets_lower,
+        asset_weights <= assets_upper,
+        cash_weight >= cash_lower,
+        cash_weight <= cash_upper,
+    ]
 
-    risk = cp.norm2(cp.hstack([cp.norm2(chol_f.T @ f), cp.norm2(cp.multiply(idio_vola, w[:-1]))]))
 
-    cons = [cp.sum(w) == 1, risk <= sigma_tar / np.sqrt(250), f == exposure.T @ w[:-1]]
 
-    if lower is not None:
-        cons.append(w[:-1] >= lower)
-    if upper is not None:
-        cons.append(w[:-1] <= upper)
-    if leverage is not None:
-        cons.append(cp.norm1(w[:-1]) <= leverage)
 
-    if lower_cash is not None:
-        cons.append(w[-1] >= lower_cash)
-    if upper_cash is not None:
-        cons.append(w[-1] <= upper_cash)
 
-    prob = cp.Problem(obj, cons)
 
-    return (
-        prob,
-        w,
-        f,
-        alpha_w,
-        alpha_f,
-        exposure,
-        chol_f,
-        idio_vola,
-        trading_costs,
-        trading_costs_times_w_old,
-        w_old,
-    )
+
+
