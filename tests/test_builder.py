4251240d1499ba0a009c8adebb20c5fa2d94d8bd
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-09-14 15:57:37 -0700

    testing with something to cover (#9)
    * testing with something to cover
    
    * testing with something to cover
    
    * remove coverage
    
    * remove coverage from basic workflow
    
    * remove sphinx documentation
    
    * remove test reports from book
    
    * Revisit the notebook for the book
    
    * Revisit the notebook for the book

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,5 +1,18 @@
 # """
 # testing the builder
 # """
+from cvx.markowitz.backtest import ParameterizedProblem
+
+
 def test_trivial(prices):
     assert prices.shape == (602, 7)
+
+
+def test_problem():
+    p = ParameterizedProblem(
+        problem="Markowitz",
+        variables=["x", "y"],
+        parameters="x + y",
+    )
+
+    assert p
