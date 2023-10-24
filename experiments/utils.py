e1b04620183d096f7e16c9214e957dddba24a5bd
kasperjo <77122345+kasperjo@users.noreply.github.com>
2023-10-24 15:35:11 -0700

    18 synthetic returns (#24)
    * typo
    
    * synthetic returns
    
    * removed playground
    
    * merged long-only and unconstrained
    
    * remove uncommented function
    
    * explained return predictions model
    
    * Small updates
    
    ---------
    
    Co-authored-by: phschiele <phschiele@gmail.com>

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,8 +1,28 @@
 import numpy as np
+import pandas as pd
 
 
-def synthetic_returns(prices, sigma_r=0.02236, sigma_eps=0.14142):
+def synthetic_returns(
+    prices: pd.DataFrame, var_r: float = 0.0005, var_eps: float = 0.02
+) -> pd.DataFrame:
+    """
+    param prices: a DataFrame of prices
+    param var_r: the Gaussian variance of the returns
+    param var_eps: the Gaussian variance of the noise term
+
+    returns: a DataFrame of "synthetic return predictions" computed as
+    alpha*(returns+noise), where alpha=var_r / (var_r + var_eps); this is the
+    coefficient that minimize the variance of the prediction error under the
+    above model.
+
+    var_r = 0.0005 and var_eps = 0.02 correspond to an information ratio
+    sqrt(alpha) of about 0.15.
+    """
     returns = prices.pct_change()
 
-    alpha = sigma_r**2 / (sigma_r**2 + sigma_eps**2)
-    return alpha * (returns + np.random.normal(size=returns.shape) * sigma_eps)
+    alpha = var_r / (var_r + var_eps)
+    sigma_eps = np.sqrt(var_eps)
+    synthetic_returns = alpha * (
+        returns + np.random.normal(size=returns.shape) * sigma_eps
+    )
+    return synthetic_returns
