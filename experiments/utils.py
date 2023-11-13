b5670a5c60861482c0c787455c59bcbfdef22b29
phschiele <phschiele@gmail.com>
2023-11-13 19:32:39 +0100

    Taming

--- experiments/utils.py
+++ experiments/utils.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def synthetic_returns(prices: pd.DataFrame, information_ratio: float) -> pd.DataFrame:
+def synthetic_returns(
+    prices: pd.DataFrame, information_ratio: float, forward_smoothing: int
+) -> pd.DataFrame:
     """
     prices: a DataFrame of prices
     information_ratio: the desired information ratio of the synthetic returns
@@ -12,11 +14,31 @@ def synthetic_returns(prices: pd.DataFrame, information_ratio: float) -> pd.Data
     coefficient that minimize the variance of the prediction error under the
     above model.
     """
+    np.random.default_rng(1)
+
     returns = prices.pct_change()
+    returns = returns.rolling(forward_smoothing).mean().shift(-(forward_smoothing - 1))
     var_r = returns.var()
 
     alpha = information_ratio**2
     var_eps = var_r * (1 - alpha) / alpha
     noise = np.random.normal(0, np.sqrt(var_eps), size=returns.shape)
     synthetic_returns = alpha * (returns + noise)
+
     return synthetic_returns
+
+
+if __name__ == "__main__":
+    prices = pd.read_csv("data/prices.csv", index_col=0, parse_dates=True)
+    synthetic_returns = synthetic_returns(
+        prices, information_ratio=0.15, forward_smoothing=5
+    )
+    returns = prices.pct_change()
+    print(
+        (
+            np.sign(synthetic_returns - returns.mean())
+            == np.sign(returns - returns.mean())
+        )
+        .mean(axis=0)
+        .describe()
+    )
