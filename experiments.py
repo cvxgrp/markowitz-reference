39e345c369c16fcde871ca74caea489b2a17a5ae
phschiele <phschiele@gmail.com>
2023-12-13 01:21:43 +0100

    Make figures dir

--- experiments.py
+++ experiments.py
@@ -5,5 +5,6 @@ from experiments.scaling_small import main as scaling_small_main
 
 if __name__ == "__main__":
     Path("checkpoints").mkdir(exist_ok=True)
+    Path("figures").mkdir(exist_ok=True)
     scaling_small_main()
     taming_main()
