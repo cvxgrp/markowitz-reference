53aec9522a7ed12401dfd03fbed117a56d479f69
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2024-01-01 09:52:24 +0400

    Check if running on a CI/CD server

--- experiments.py
+++ experiments.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from loguru import logger
@@ -8,6 +9,12 @@ from experiments.scaling_large import main as scaling_large_main
 
 if __name__ == "__main__":
     logger.info("Welcome to ...")
+
+    # check if on a Github server where a Mosek license is not available
+    if os.environ.get("CI"):
+        logger.info("Running on a Github CI server")
+    else:
+        logger.info("Running on a local machine")
 
     logger.debug("Create paths for checkpoints and figures")
     Path("checkpoints").mkdir(exist_ok=True)
